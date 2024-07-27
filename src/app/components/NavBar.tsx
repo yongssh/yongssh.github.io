6206b07bc7c94df8cfff7994ca715a6e72b18d16
yongssh <yongyu.yy.huang@gmail.com>
2024-07-26 19:46:56 -0500

    changing about page for mobile

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,18 +1,14 @@
 "use client"; // Add this directive at the top
-
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { EB_Garamond } from 'next/font/google'
-import styles from '../styles/Introduction.module.css'; 
-import WritingPage from "../writing/page";
+import { EB_Garamond } from 'next/font/google';
 import { HiMenu, HiX } from 'react-icons/hi';
-import { useState } from "react";
+import styles from '../styles/Introduction.module.css';
 
 const garamond = EB_Garamond({ 
   subsets: ['latin'],
   variable: '--font-garamond'
-})
-
+});
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,38 +16,58 @@ const NavBar = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <div className={`w-full h-20 bg-sky-950 sticky top-0 z-50 ${garamond.className}`}>
-      <div className="container mx-auto px-4 h-full flex justify-between items-center">
-        <div>
-          <p className="text-white m-0 text-lg font-bold">Yong-Yu Huang</p>
-        </div>
-        <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
-            {isOpen ? <HiX size={24} /> : <HiMenu size={24} />}
-          </button>
-        </div>
-        <div className={`md:flex gap-x-6 text-white ${isOpen ? 'block' : 'hidden'} md:block`}>
-          <ul className="flex flex-col md:flex-row">
+    <>
+      <div className={`w-full h-20 bg-sky-950 sticky top-0 ${garamond.className}`}>
+      <div className="flex justify-between items-center h-full px-4">
+          <p className="text-white m-0">Yong-Yu Huang</p>
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="text-white">
+              {isOpen ? <HiX size={30} /> : <HiMenu size={30} />}
+            </button>
+          </div>
+          <ul className="hidden md:flex gap-x-6 text-white">
             <li>
               <Link href="/about">
-                <p className="py-2 md:py-0">About</p>
+                <p>About</p>
               </Link>
             </li>
             <li>
               <Link href="/writing">
-                <p className="py-2 md:py-0">Writing</p>
+                <p>Writing</p>
               </Link>
             </li>
             <li>
               <Link href="/photo-gallery">
-                <p className="py-2 md:py-0">Photography</p>
+                <p>Photography</p>
               </Link>
             </li>
           </ul>
         </div>
+        {isOpen && (
+          <div className="md:hidden bg-sky-950 text-white">
+            <ul className="flex flex-col items-center">
+              <li className="py-2">
+                <Link href="/about" onClick={toggleMenu}>
+                  <p>About</p>
+                </Link>
+              </li>
+              <li className="py-2">
+                <Link href="/writing" onClick={toggleMenu}>
+                  <p>Writing</p>
+                </Link>
+              </li>
+              <li className="py-2">
+                <Link href="/photo-gallery" onClick={toggleMenu}>
+                  <p>Photography</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
