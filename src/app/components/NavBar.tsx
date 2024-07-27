a42d04bf174d7588fedb409ce8aeb384c6c2af53
yongssh <yongyu.yy.huang@gmail.com>
2024-07-26 19:41:10 -0500

    changing about page for mobile

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,8 +1,12 @@
+"use client"; // Add this directive at the top
+
 import React from "react";
 import Link from "next/link";
 import { EB_Garamond } from 'next/font/google'
 import styles from '../styles/Introduction.module.css'; 
 import WritingPage from "../writing/page";
+import { HiMenu, HiX } from 'react-icons/hi';
+import { useState } from "react";
 
 const garamond = EB_Garamond({ 
   subsets: ['latin'],
@@ -11,38 +15,44 @@ const garamond = EB_Garamond({
 
 
 const NavBar = () => {
-    return (
-      <>
-        <div className={`w-full h-20 bg-sky-950 sticky top-0 ${garamond.className}`}>
-          <div className="container mx-auto px-4 h-full">
-            <div className="flex justify-between items-center h-full">
-              <div>
-                <p className="text-white m-0">Yong-Yu Huang</p>
-              </div>
-              <div>
-                <ul className="hidden md:flex gap-x-6 text-white">
-                  <li>
-                    <Link href="/about">
-                      <p>About</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/writing">
-                      <p>Writing</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/photo-gallery">
-                      <p>Photography</p>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
-  
-  export default NavBar;
+  return (
+    <div className={`w-full h-20 bg-sky-950 sticky top-0 z-50 ${garamond.className}`}>
+      <div className="container mx-auto px-4 h-full flex justify-between items-center">
+        <div>
+          <p className="text-white m-0 text-lg font-bold">Yong-Yu Huang</p>
+        </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white focus:outline-none">
+            {isOpen ? <HiX size={24} /> : <HiMenu size={24} />}
+          </button>
+        </div>
+        <div className={`md:flex gap-x-6 text-white ${isOpen ? 'block' : 'hidden'} md:block`}>
+          <ul className="flex flex-col md:flex-row">
+            <li>
+              <Link href="/about">
+                <p className="py-2 md:py-0">About</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/writing">
+                <p className="py-2 md:py-0">Writing</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/photo-gallery">
+                <p className="py-2 md:py-0">Photography</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { EB_Garamond } from 'next/font/google';
-import styles from '../styles/Introduction.module.css'; // Import CSS module
+import styles from '../styles/Introduction.module.css';
 import Image from "next/image";
-
 
 const garamond = EB_Garamond({ 
   subsets: ['latin'],
   variable: '--font-garamond'
 });
 
-/*<div>There is language that has transformed the way I look at <a className="text-decoration: underline" href="https://www.poetryfoundation.org/poems/44399/pied-beauty">light as it falls on swimming trout</a>,&thinsp;
-<a className="text-decoration: underline"  href="https://www.poetryfoundation.org/poems/56657/touch-gallery-joan-of-arc">whole months</a>,&thinsp;<a className="text-decoration: underline" href="http://www.phys.unm.edu/~tw/fas/yits/archive/oliver_wildgeese.html">the geese stalking around campus.</a>  
-Language has the capacity to help an ordinary thing  take on new dimensions, catch the light differently. </div>*/
 const AboutSection = () => {
-    return (
-      <section className={`bg-white text-black ${garamond.className} ${styles.IntroSection}`}>
-        <div className="flex flex-row items-center">
-          <div className="mr-8 ml-12 mt-12"> {/* Adjust width and padding as needed */}
-            <Image 
-              src="/portrait.JPG" // Adjust path to your image
-              alt="portrait image"
-              width={300}
-              height={400}
-            />
-          </div>
-          <p className={`about-bio mt-8 mr-8 flex-1`}> {/* Adjust margin-left (ml-4) as needed */}
-            <span>Yong-Yu Huang studies English literature and computer science at Northwestern University. She has been a Kaplan Scholar and a Leopold Fellow, and she has won the William Faricy Poetry Award, the Helen G. Scott Essay Prize, and the Kaplan Humanities Scholars Prize. 
-              Her work appears in <em>Waxwing</em>, <em>The Adroit Journal</em>, <em>The Offing</em>, <em>The Penn Review</em>, and elsewhere. She is the recipient of the 2021 Elinor Benedict Poetry Prize and has been recognized by the Poetry Society of the UK, Best Small Fictions, the Hippocrates Society, and the Gregory Djanikian Scholars Program, among others.
-              In her free time, she can be found doing crosswords, listening to Lorde, or going for a walk.
-            </span>
-            <br></br>
-            <br></br>
-           <span> <a className='text-decoration: underline' href="mailto:yongyu.yy.huang@gmail.com">Email</a><span>, </span><a className='text-decoration: underline' href="https://www.twitter.com/yong_yuhuang">Twitter</a><span>, or read more on </span><a className='text-decoration: underline' href="https://yongyuhuang.substack.com/">Substack</a>.
-           </span>
-          </p>
+  return (
+    <section className={`bg-white text-black p-4 ${garamond.className} ${styles.IntroSection}`}>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
+        <div className="md:col-span-1 flex justify-center md:justify-start">
+          <Image 
+            src="/portrait.JPG" 
+            alt="portrait image"
+            width={300}
+            height={400}
+            className="w-full h-auto max-w-xs md:max-w-full"
+          />
         </div>
-      </section>
-    );
-  };
-  
-  export default AboutSection;
+        <p className="md:col-span-2 text-center md:text-left mt-4 md:mt-0">
+          <span>
+            Yong-Yu Huang studies English literature and computer science at Northwestern University. She has been a Kaplan Scholar and a Leopold Fellow, and she has won the William Faricy Poetry Award, the Helen G. Scott Essay Prize, and the Kaplan Humanities Scholars Prize. 
+            Her work appears in <em>Waxwing</em>, <em>The Adroit Journal</em>, <em>The Offing</em>, <em>The Penn Review</em>, and elsewhere. She is the recipient of the 2021 Elinor Benedict Poetry Prize and has been recognized by the Poetry Society of the UK, Best Small Fictions, the Hippocrates Society, and the Gregory Djanikian Scholars Program, among others.
+            In her free time, she can be found doing crosswords, listening to Lorde, or going for a walk.
+          </span>
+          <br></br>
+          <br></br>
+          <span>
+            <a className='underline' href="mailto:yongyu.yy.huang@gmail.com">Email</a>
+            <span>, </span>
+            <a className='underline' href="https://www.twitter.com/yong_yuhuang">Twitter</a>
+            <span>, or read more on </span>
+            <a className='underline' href="https://yongyuhuang.substack.com/">Substack</a>.
+          </span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
