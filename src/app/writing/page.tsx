b8d49e7cc8a0bbcfec780f060a62e500c4c9b109
yongssh <yongyu.yy.huang@gmail.com>
2024-07-27 01:55:16 -0500

    fixed home page

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import Publications from '../components/publications';
-import Honors from '../components/honors';
+import AboutSection from '../components/about';
 import Footer from '../components/footer';
+
 
 const WritingPage = () => {
   return (
+    
     <div className="wrapper bg-white text-black">
+            <main className="flex-grow">
+
       <NavBar />
-      <main className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <Publications />
-          <Honors />
-        </div>
+      <AboutSection />
       </main>
       <Footer />
+
     </div>
+    
   );
 };
 
