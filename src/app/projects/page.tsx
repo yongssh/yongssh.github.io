42670856ec4958c8c182547d29076442852adcfc
yongssh <yongyu.yy.huang@gmail.com>
2025-10-03 00:01:40 -0500

    added projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,19 +6,19 @@ import Link from "next/link";
 const projects = [
   {
     title: "EPL Story with Tax Calculator",
-    description: "Reported and built a tax calculator widget with interactive data visualizations for the Evanston Public Library debate.",
+    description: "Reported and built a tax calculator widget with interactive data visualizations with D3.js for the Evanston Public Library debate.",
     image: "/epl.png",
     url: "https://dailynorthwestern.com/2025/10/01/top-stories/not-just-a-place-of-books-evanston-public-library-debates-potential-split-from-city/"
   },
   {
     title: "Migration in Mohsin Hamid's Exit West",
-    description: "Created interactive scrolly-telling page to track Hamid's characters as they travel across the world.",
+    description: "Created interactive scrolly-telling page to track Hamid's characters as they travel across the world, using Leaflet.js",
     image:"/lahore-map.png",
     url:"https://yongssh.github.io/exit-west.html"
-  }
+  },
   {
-    title: "Daily Northwestern Election Page",
-    description: "Designed and developed interactive election map and visuals for election coverage.",
+    title: "Daily Northwestern Elections Page",
+    description: "Designed and developed interactive election map with D3.js for election coverage, using WaPo's React component library.",
     image: "/schapiroschill.png", 
     url: "#" 
   },
@@ -30,13 +30,13 @@ const projects = [
   },
   {
     title: "Chicago Heatwave Project",
-    description: "Created live data visualizations and reported during the 1995 heatwave event screening.",
+    description: "Created data visualizations and reported during the 1995 heatwave event screening.",
     image: "/johnson.png", 
     url: "https://dailynorthwestern.com/2025/07/16/city/chicago-mayor-brandon-johnson-buffett-researchers-host-screening-and-discussion-on-1995-heat-wave/"
   },
   {
     title: "Northwestern University Executives in Numbers",
-    description: "Developed interactive data visualizations to support reporting on university executive compensation.",
+    description: "Developed interactive data visualizations to support reporting on university executive compensation with Flourish.",
     image: "/schapiroschill.png.png",
     url: "https://dailynorthwestern.com/2025/09/15/campus/outgoing-university-president-michael-schill-earned-nearly-1-7-million-in-first-full-year-tax-filings-show/"
   },
