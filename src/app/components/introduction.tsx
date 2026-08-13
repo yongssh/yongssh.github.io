d38f870d742759050b57e7d7cc3b6365c10291d2
yongssh <yongyu.yy.huang@gmail.com>
2026-08-13 14:13:44 -0700

    updates

--- src/app/components/introduction.tsx
+++ src/app/components/introduction.tsx
@@ -1,25 +1,71 @@
-import React from "react";
-import { EB_Garamond } from 'next/font/google';
-import styles from '../styles/Introduction.module.css';
-import Image from "next/image";
+"use client";
 
+import React, { useEffect, useState } from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import styles from "../styles/About.module.css";
 
-const IntroSection = () => {
-    return (
-        <section className={`bg-white text-black font-zen-old-mincho} ${styles.IntroSection}`}>
-            <p className="bio">
-                I study English literature and computer science at Northwestern University. In my free time, you can find me doing a crossword or going for a walk.
-            </p>
-            <div className="col-span-5">
-                <Image 
-                    src="/hike.png"
-                    alt="hike image"
-                    width={200}
-                    height={150}
-                />
-            </div>
-        </section>
-    );
-};
+export default function About() {
+  const [visibleIndex, setVisibleIndex] = useState(0);
 
-export default IntroSection;
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setVisibleIndex((prev) => (prev < 2 ? prev + 1 : prev));
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <section className={styles.aboutSection} aria-labelledby="about-heading">
+      <header className={styles.header}>
+        <h1 id="about-heading" className={styles.name}>
+          Yong-Yu Huang
+        </h1>
+
+        <div className={styles.iconRow}>
+          <a
+            href="https://www.linkedin.com/in/yong-yuhuang/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/yongssh"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+        </div>
+      </header>
+
+      <article className={styles.bioContainer}>
+        <p
+          className={`${styles.bioText} ${
+            visibleIndex >= 0 ? styles.visible : ""
+          }`}
+        >
+          Yong-Yu Huang studied English literature and computer science at Northwestern University. Currently, she is a data and reporting fellow at <i><a href="https://www.rangemedia.co/author/yong-yuhuang/">RANGE Media</a></i> in Spokane, Washington. 
+          Her work can be found in <i>The Nation</i>, <i>Britannica</i>, <i>Penang Monthly</i>, and <i>The Daily Northwestern</i>.
+          <br></br>
+          Her poery is featured in <i>Narrative Magazine</i>, <i>Poet Lore</i>, and<i>The Adroit Journal</i>, among others, and has been recognized by The Poetry Society of the UK. <u><a href="https://www.printedmatter.org/catalog/71152">Here&apos;s</a></u> a zine of her poetry and photography, made in collaboration with Kimberly Jao.
+        </p>
+        <p
+          className={`${styles.bioText} ${
+            visibleIndex >= 2 ? styles.visible : ""
+          }`}
+        >
+          <a href="mailto:yongyu.yy.huang@gmail.com">
+            <u>Email</u>
+          </a>
+          {" "}or{" "}
+          <a href="https://www.twitter.com/yong_yuhuang">
+            <u>Twitter</u>
+          </a>.
+        </p>
+      </article>
+    </section>
+  );
+}
